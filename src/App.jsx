e08633b2a5f2c236c5cdbfc883c42ef3bd90eb72
abdurrahimalim <abdurrahim@student.ub.ac.id>
2026-03-28 20:21:31 +0700

    feat: initial React + Tailwind + Router setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProfilAbdurrahim from './pages/ProfilAbdurrahim';
+import ProfilShafa from './pages/ProfilShafa';
+import ProfilAzkiya from './pages/ProfilAzkiya';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/abdurrahim'    element={<ProfilAbdurrahim />} />
+        <Route path='/shafa-kamalia' element={<ProfilShafa />} />
+        <Route path='/azkiya-faza'  element={<ProfilAzkiya />} />
+        <Route path='/'             element={<ProfilAbdurrahim />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',  // scan semua file di src/
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
