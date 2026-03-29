6afa3f4187d6b4aada0a81fcf19966181557c57b
Azkiya-12 <azkiyafazaputripratama@gmail.com>
2026-03-29 10:48:06 +0700

    feat: tambah ProfilAzkiya.jsx

---
+++ src/pages/ProfilAzkiya.jsx
@@ -0,0 +1,43 @@
+function ProfilAzkiya(){
+    return (
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md w-full text-center">
+
+                <img
+                src='/foto-azkiya.jpg'
+                alt='foto Azkiya'
+                className="w-32 h-32 rounded-full mx-auto mb-4 object-cover"
+                />
+
+                <h1 className="text-2xl font-bold text-gray-800">Azkiya Faza Putri Pratama</h1>
+
+                <p className="text-blue-200 text-lg">NIM: 253140707111014</p>
+                <p className="text-gray-600 mt-2">Kelas: T2D</p>
+                <p className="text-gray-600">Jurusan: Teknologi Informasi (D3)</p>
+                <p className="text-gray-600">Domisili: Kalimantan Selatan, Tanah Bumbu</p>
+
+                <div className="mt-4 text-gray-700 text-sm text-justify">
+                    <p>
+                        Saya adalah seorang mahasiswa di Universitas Brawijaya, Fakultas Vokasi,
+                    jurusan Teknologi Informasi D3. Saat ini saya sedang belajar di bidang Teknologi,
+                    khususnya dalam pengembangan website. Saya senang bisa mempelajari hal-hal baru
+                    yang berkaitan dengan IT untuk terus meningkatkan kemampuan yang saya miliki.
+                    Selama proses belajar saya akan terus mencoba agar dapat lebih memahami tentang
+                    dunia IT.
+
+                    Dalam proses belajar, saya akan terus berusaha untuk memahami materi dan terus
+                    mencoba agar dapat menguasai apa yang telah dipelajari. Saya juga berusaha untuk
+                    konsisten dan tidak mudah menyerah saat menghadapi kesulitan. Bagi saya, belajar
+                    adalah proses yang penting untuk mencapai tujuan di masa depan dan dapat mengembangkan
+                    potensi diri secara maksimal.
+
+                    Saya berharap kedepannya dapat terus mengembangkan kemampuan yang saya miliki dan menjadi
+                    pribadi yang lebih baik di bidang teknologi.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProfilAzkiya;
